b258e717a5e3a64fa7615836913be8c09613b3da
Dave <davidglogowski2003@gmail.com>
2025-10-14 17:22:03 +0200

    style(exercise): make code container smaller for mobile devices

--- src/features/Exercise/ExerciseCodeContainer.tsx
+++ src/features/Exercise/ExerciseCodeContainer.tsx
@@ -8,7 +8,7 @@ export function ExerciseCodeContainer({
   children,
 }: ExerciseCodeContainerProps) {
   return (
-    <div className="w-full h-80 rounded-4xl bg-ludoGrayLight">
+    <div className="w-full min-h-46 rounded-4xl bg-ludoGrayLight">
       <div className="w-full h-10 rounded-t-4xl bg-ludoYellow"></div>
       <div className="w-full h-full p-6">
         {children}
