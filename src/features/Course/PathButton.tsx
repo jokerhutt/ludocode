99005658a4cdd77fad06678f3ab0adee72304639
Dave <davidglogowski2003@gmail.com>
2025-10-14 17:18:45 +0200

    misc(icons): Add Hero Icons

--- src/features/Course/PathButton.tsx
+++ src/features/Course/PathButton.tsx
@@ -12,7 +12,7 @@ export function PathButton() {
   return (
     <button
       className="relative inline-flex items-center justify-center
-                 w-20 h-20 rounded-3xl bg-gray-700 overflow-hidden
+                 w-20 h-20 rounded-3xl bg-ludoGrayLight overflow-hidden
                  shadow-[0_10px_0_#1F2328] active:translate-y-2 active:shadow-none"
     >
     <CompletionRibbon lessonState={lessonType}/>
