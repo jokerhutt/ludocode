3e2568b0ffea5bbdd685a2422f8f017ebc8b5be8
Dave <davidglogowski2003@gmail.com>
2025-10-29 00:11:42 +0100

    feat(form): Add module form fields

--- src/components/Atoms/Row/ListRow.tsx
+++ src/components/Atoms/Row/ListRow.tsx
@@ -5,7 +5,7 @@ type ListRowProps = {
   children: ReactNode;
   active?: boolean;
   onClick?: () => void;
-  alignment?: "start" | "center" | "end"
+  alignment?: "start" | "center" | "end" | "between"
   fill?: boolean
   py?: string
   px?: string
@@ -14,13 +14,14 @@ type ListRowProps = {
 export function ListRow({hover, children, active, onClick, alignment = "start", fill, py = "py-4", px = "px-2"}: ListRowProps) {
 
   const hoverStyle = hover ? "hover:bg-ludoGrayLight/20" : ""
-  const activeStyle = active ? "bg-white/30" : ""
+  const activeStyle = active ? "bg-white/5" : ""
   const bg = fill ? "bg-ludoGrayLight border-b-ludoGrayDark" : "border-b-ludoGrayLight"
 
   const orientation = {
     start: "justify-start",
     center: "justify-center",
-    end: "justify-end"
+    end: "justify-end",
+    between: "justify-between"
   }
 
   return (
--- src/features/Builder/Forms/ModuleForm.tsx
+++ src/features/Builder/Forms/ModuleForm.tsx
@@ -8,7 +8,6 @@ import {
 import { AsideComponent } from "../../../Layouts/Aside/AsideComponent";
 import { ludoNavigation } from "../../../routes/ludoNavigation";
 import { router } from "../../../routes/router";
-import type { ModuleSnapshot } from "../../../Types/Snapshot/SnapshotTypes";
 
 export const ModuleForm = withForm({
   ...courseFormOpts,
@@ -21,25 +20,47 @@ export const ModuleForm = withForm({
       <form.Field
         name="modules"
         mode="array"
-        children={(modules) => (
+        children={(fieldArray) => (
           <AsideComponent customSpan="col-start-1 col-end-4" orientation="LEFT">
             <div className="flex flex-col py-6">
               <ListContainer title="Modules">
-                {modules.state.value.map((module: ModuleSnapshot) => (
-                  <ListRow
-                    onClick={() =>
-                      router.navigate(
-                        ludoNavigation.build.toBuilder(
-                          courseId,
-                          module.moduleId
-                        )
-                      )
-                    }
-                    key={module.moduleId}
-                    active={moduleId === module.moduleId}
-                  >
-                    <p>{module.title}</p>
-                  </ListRow>
+                {fieldArray.state.value.map((_, index) => (
+                  <form.Field
+                    name={`modules[${index}]`}
+                    children={(subField) => (
+                      <ListRow
+                        alignment="between"
+                        onClick={() =>
+                          router.navigate(
+                            ludoNavigation.build.toBuilder(
+                              courseId,
+                              subField.state.value.moduleId
+                            )
+                          )
+                        }
+                        key={subField.state.value.moduleId}
+                        active={moduleId === subField.state.value.moduleId}
+                        px="px-2"
+                      >
+                        <div className=" w-full flex gap-2 flex-col">
+                          <p>{subField.state.value.title}</p>
+                          <select
+                            className="border-ludoLightPurple border-2 rounded-md w-20"
+                            value={`${index}`}
+                            onChange={(e) =>
+                              fieldArray.moveValue(index, Number(e.target.value))
+                            }
+                          >
+                            {fieldArray.state.value.map((_, index) => (
+                              <option key={index} value={`${index}`}>
+                                # {index}
+                              </option>
+                            ))}
+                          </select>
+                        </div>
+                      </ListRow>
+                    )}
+                  />
                 ))}
                 <ListRow alignment="center" fill={true} py="py-2">
                   <p className="text-center text-xl font-bold">+</p>
