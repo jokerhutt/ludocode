962a4dc7f8bcab9ee94121233f0be4a99b70c484
Dave <davidglogowski2003@gmail.com>
2025-11-08 00:54:55 +0100

    feat(exit lesson): Add exit button atom

--- src/features/Tutorial/TutorialHeader.tsx
+++ src/features/Tutorial/TutorialHeader.tsx
@@ -1,5 +1,7 @@
+import { HeroIcon } from "@/components/Atoms/Icons/HeroIcon";
 import { CommonHeader } from "../../components/Molecules/Header/CommonHeader";
 import { SegmentedProgress } from "../../components/Molecules/Progress/SegmentedProgress";
+import { ExitButton } from "@/components/Atoms/Button/ExitButton";
 
 type TutorialHeaderProps = {
   total: number;
@@ -12,6 +14,9 @@ export function TutorialHeader({ total, position }: TutorialHeaderProps) {
 
   return (
     <CommonHeader device="Both">
+      <div className="col-start-1 col-end-3 flex justify-center items-center h-full">
+        <ExitButton/>
+      </div>
       <div className="flex items-center justify-center col-start-2 col-end-12 lg:col-start-3 lg:col-end-11">
         <SegmentedProgress total={total} completed={completed} />
       </div>
