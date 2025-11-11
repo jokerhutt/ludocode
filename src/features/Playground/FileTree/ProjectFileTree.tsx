e93d83c70e784bea586993ce807637bbb6d4c21d
Dave <davidglogowski2003@gmail.com>
2025-11-11 21:47:16 +0100

    style(file tree): Add new file button

--- src/features/Playground/FileTree/ProjectFileTree.tsx
+++ src/features/Playground/FileTree/ProjectFileTree.tsx
@@ -1,11 +1,26 @@
 import type { ProjectFile } from "@/Hooks/Logic/Playground/useProject";
 import { TreeFile } from "./TreeFile";
+import { PlusIcon, XIcon } from "lucide-react";
 
-type ProjectFileTreeProps = { projects: ProjectFile[]; current: number, changeFile: (index: number) => void; };
+type ProjectFileTreeProps = {
+  projects: ProjectFile[];
+  current: number;
+  changeFile: (index: number) => void;
+};
 
-export function ProjectFileTree({ projects, current, changeFile }: ProjectFileTreeProps) {
+export function ProjectFileTree({
+  projects,
+  current,
+  changeFile,
+}: ProjectFileTreeProps) {
   return (
     <div className="flex bg-ludoGrayDark gap-2 text-white flex-col w-full">
+      <div className="flex mb-2 justify-between items-center">
+        <p>Files</p>
+        <div className="p-0.5 hover:cursor-pointer hover:bg-ludoLightPurple/80 rounded-full">
+          <PlusIcon className="h-4 w-4" />
+        </div>
+      </div>
       {projects.map((project, index) => (
         <TreeFile
           onClick={() => changeFile(index)}
