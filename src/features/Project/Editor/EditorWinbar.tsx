09fe073a8f6f6d6a566a900cecbac0b762848a5a
Dave <davidglogowski2003@gmail.com>
2025-11-23 18:33:23 +0100

    refactor(editor tabs): Extract editor tabs group as component (#54)

--- src/features/Project/Editor/EditorWinbar.tsx
+++ src/features/Project/Editor/EditorWinbar.tsx
@@ -2,19 +2,20 @@ import type { ProjectFile } from "@/Hooks/Logic/Playground/useProject.tsx";
 import { ProjectWinbar } from "../ProjectWinbar.tsx";
 import { stripFileName } from "@/Hooks/Logic/Playground/playgroundFileUtils.ts";
 import { EditorTab } from "@/components/Atoms/Tab/EditorTab.tsx";
+import { EditorTabGroup } from "@/components/Molecules/Group/EditorTabGroup.tsx";
 
 type EditorWinbarProps = { current: number; files: ProjectFile[] };
 
 export function EditorWinbar({ current, files }: EditorWinbarProps) {
   return (
     <ProjectWinbar>
-      <div className="flex h-full pt-2 px-6 items-center">
+      <EditorTabGroup>
         {current !== null && current !== undefined && (
           <EditorTab>
             <p>{stripFileName(files[current].path)}</p>
           </EditorTab>
         )}
-      </div>
+      </EditorTabGroup>
     </ProjectWinbar>
   );
 }
