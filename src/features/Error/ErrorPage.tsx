367599012fe2fe1691b28a54067323ea76469f34
Dave <davidglogowski2003@gmail.com>
2025-11-24 19:26:34 +0100

    feat(errors): Add fallback action to errors -e.g. navigate home

--- src/features/Error/ErrorPage.tsx
+++ src/features/Error/ErrorPage.tsx
@@ -4,18 +4,18 @@ import { errorMap, type ErrorStatus } from "@/constants/static-data/ErrorData";
 type ErrorPageProps = { errorCode: ErrorStatus };
 
 export function ErrorPage({ errorCode }: ErrorPageProps) {
-  const { status, title, suggestion, actionText } = errorMap[errorCode];
+  const { status, title, suggestion, actionText, fallbackAction } = errorMap[errorCode];
 
   return (
-    <div className="w-dvw h-dvh flex flex-col gap-2 items-center justify-center bg-ludoGrayDark">
+    <div className="w-dvw h-dvh flex flex-col gap-4 items-center justify-center bg-ludoGrayDark">
       <h1 className="text-center text-6xl text-ludoAltText">{status}</h1>
-      <div className="p-6 text-center flex flex-col gap-6 rounded-lg bg-ludoGrayLight">
+      <div className="p-6 text-center flex flex-col gap-6 rounded-lg bg-ludoGrayLight/70">
         <div className="flex flex-col gap-2">
           <p className="text-ludoAltText">{title}</p>
           <p className="text-ludoAltText">{suggestion}</p>
         </div>
 
-        <Button>{actionText}</Button>
+        <Button onClick={() => fallbackAction?.()}>{actionText}</Button>
       </div>
     </div>
   );
