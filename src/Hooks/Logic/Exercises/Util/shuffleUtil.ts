da09c9a0b53a557e6e3cf751cb95dff7779cf49c
Dave <davidglogowski2003@gmail.com>
2025-11-29 19:31:03 +0100

    feat(exercise options): Shuffle order of options for user

---
+++ src/Hooks/Logic/Exercises/Util/shuffleUtil.ts
@@ -0,0 +1,6 @@
+export const shuffleArray = <T>(arr: T[]): T[] => {
+  return arr
+    .map((item) => ({ item, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ item }) => item);
+};
--- src/Hooks/Logic/Exercises/useExerciseBodyData.tsx
+++ src/Hooks/Logic/Exercises/useExerciseBodyData.tsx
@@ -1,12 +1,24 @@
 import type { LudoExercise } from "@/Types/Exercise/LudoExercise";
 import type { useExerciseInputResponse } from "./useExerciseInput";
+import { useMemo } from "react";
+import { shuffleArray } from "./Util/shuffleUtil";
 
 export function useExerciseBodyData(
   currentExercise: LudoExercise,
   inputState: useExerciseInputResponse
 ) {
   const { prompt, correctOptions, distractors } = currentExercise;
-  const options = [...correctOptions, ...distractors];
+
+  const mergedOptions = useMemo(
+    () => [...correctOptions, ...distractors],
+    [currentExercise.id]
+  );
+
+  const options = useMemo(
+    () => shuffleArray(mergedOptions),
+    [mergedOptions]
+  );
+
   const { currentExerciseInputs, replaceAnswerAt, setAnswerAt } = inputState;
 
   return {
