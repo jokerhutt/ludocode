1f4ed384e445d242f0292f1dc44ecfd9aa753f10
Dave <davidglogowski2003@gmail.com>
2026-01-07 23:51:56 +0100

    fix(onboarding): fix onboarding mock paths

--- apps/web/src/constants/mocks/onboardingMocks.ts
+++ apps/web/src/constants/mocks/onboardingMocks.ts
@@ -3,14 +3,14 @@ import type { OnboardingFormContent } from "@ludocode/types";
 export const onboardingContent: OnboardingFormContent = {
   courseContent: [
     {
-      courseId: "598ccbea-4957-4569-81cb-ea901b62c329",
+      courseId: "75975805-3f02-43c2-9106-c990d944dfd2",
       title: "Python",
       description: "Python is a simple programming language",
     },
   ],
   careerContent: [
     {
-      courseId: "598ccbea-4957-4569-81cb-ea901b62c329",
+      courseId: "75975805-3f02-43c2-9106-c990d944dfd2",
       title: "Data science",
       careerType: "DATA",
       description: "Data scientists",
