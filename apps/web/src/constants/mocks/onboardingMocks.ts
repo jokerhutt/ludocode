119cde2a9a8887ab7f466c9b875d7fd968238ec7
Dave <davidglogowski2003@gmail.com>
2026-01-07 02:03:01 +0100

    chore(onboarding): Update mock courseId

--- apps/web/src/constants/mocks/onboardingMocks.ts
+++ apps/web/src/constants/mocks/onboardingMocks.ts
@@ -3,14 +3,14 @@ import type { OnboardingFormContent } from "@ludocode/types";
 export const onboardingContent: OnboardingFormContent = {
   courseContent: [
     {
-      courseId: "7441852b-f392-49a5-a397-1558322385b2",
+      courseId: "75975805-3f02-43c2-9106-c990d944dfd2",
       title: "Python",
       description: "Python is a simple programming language",
     },
   ],
   careerContent: [
     {
-      courseId: "7441852b-f392-49a5-a397-1558322385b2",
+      courseId: "75975805-3f02-43c2-9106-c990d944dfd2",
       title: "Data science",
       careerType: "DATA",
       description: "Data scientists",
