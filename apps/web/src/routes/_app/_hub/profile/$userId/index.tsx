6a27da8ff9b3522c3e8fed821591ab16791f03db
Dave <davidglogowski2003@gmail.com>
2026-02-02 03:07:39 +0100

    style(headers): Add hub headers to profile & settings page

--- apps/web/src/routes/_app/_hub/profile/$userId/index.tsx
+++ apps/web/src/routes/_app/_hub/profile/$userId/index.tsx
@@ -2,5 +2,6 @@ import { ProfilePage } from "@/features/Hub/ProfileHub/Pages/ProfilePage";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_app/_hub/profile/$userId/")({
+  staticData: { headerTitle: "Profile" },
   component: ProfilePage,
 });
--- apps/web/src/routes/_app/_hub/profile/$userId/settings.tsx
+++ apps/web/src/routes/_app/_hub/profile/$userId/settings.tsx
@@ -2,5 +2,6 @@ import { AccountSettingsPage } from "@/features/Hub/ProfileHub/Pages/AccountSett
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_app/_hub/profile/$userId/settings")({
+  staticData: { headerTitle: "Settings" },
   component: AccountSettingsPage,
 });
