ed896bb168f873d891ac495e692b2fb40934363c
Dave <davidglogowski2003@gmail.com>
2026-02-04 21:02:52 +0100

    Misc(preferences): Add AI enabled checks to lessons & projects (#217)

--- apps/web/src/layouts/Project/ProjectLayout.tsx
+++ apps/web/src/layouts/Project/ProjectLayout.tsx
@@ -3,19 +3,25 @@ import { ProjectPage } from "@/features/Project/ProjectPage.tsx";
 import { getRouteApi, useLoaderData } from "@tanstack/react-router";
 import { ProjectHeader } from "@/features/Project/Zone/ProjectHeader.tsx";
 import { MainGridWrapper } from "@ludocode/design-system/layouts/grid/main-grid-wrapper.tsx";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { qo } from "@/hooks/Queries/Definitions/queries";
+import { UserPreferencesContext } from "@/hooks/Context/useUserPreferenceContext";
 
 type ProjectLayoutProps = {};
 
 export function ProjectLayout({}: ProjectLayoutProps) {
   const routeApi = getRouteApi("/_app/_desktopguard/project/$projectId");
   const { project } = useLoaderData({ from: routeApi.id });
+  const { data: UserPreferences } = useSuspenseQuery(qo.preferences());
 
   return (
-    <ProjectProvider project={project}>
-      <MainGridWrapper className="max-h-dvh min-h-0" gridRows="SITE">
-        <ProjectHeader />
-        <ProjectPage />
-      </MainGridWrapper>
-    </ProjectProvider>
+    <UserPreferencesContext.Provider value={UserPreferences}>
+      <ProjectProvider project={project}>
+        <MainGridWrapper className="max-h-dvh min-h-0" gridRows="SITE">
+          <ProjectHeader />
+          <ProjectPage />
+        </MainGridWrapper>
+      </ProjectProvider>
+    </UserPreferencesContext.Provider>
   );
 }
