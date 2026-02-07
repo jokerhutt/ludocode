61f081480a8c954e82ad281af3ea483dfc1f88fc
Dave <davidglogowski2003@gmail.com>
2026-02-07 18:09:49 +0100

    add beforeload to languages

--- apps/admin/src/routes/_app/language/$languageId.tsx
+++ apps/admin/src/routes/_app/language/$languageId.tsx
@@ -4,3 +4,5 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/_app/language/$languageId")({
   component: ModifyLanguagePage,
 });
+
+
--- apps/admin/src/routes/_app/language/route.tsx
+++ apps/admin/src/routes/_app/language/route.tsx
@@ -1,6 +1,15 @@
+import { qo } from "@/hooks/Queries/Definitions/queries";
 import { LanguageLayout } from "@/layouts/Language/LanguageLayout";
+import type { QueryClient } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_app/language")({
+  loader: async ({ context }) => languagePageLoader(context.queryClient),
   component: LanguageLayout,
 });
+
+export async function languagePageLoader(queryClient: QueryClient) {
+  await queryClient.ensureQueryData(qo.runtimes());
+  await queryClient.ensureQueryData(qo.languages());
+  await queryClient.ensureQueryData(qo.allCourses());
+}
