6f772f284491f1a6c40986715bfa2a4655de535c
Dave <davidglogowski2003@gmail.com>
2026-03-02 18:57:27 +0100

    Add demo mode banner to admin hub header

--- apps/admin/src/layouts/Hub/AdminHubHeader.tsx
+++ apps/admin/src/layouts/Hub/AdminHubHeader.tsx
@@ -1,14 +1,22 @@
 import { HeaderWithBar } from "@ludocode/design-system/zones/header-shell";
 import { AdminNavigationIconGroup } from "./AdminNavigationIconGroup";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { qo } from "@/hooks/Queries/Definitions/queries";
 
 export function AdminHubHeader() {
+  const { data: features } = useSuspenseQuery(qo.activeFeatures());
+  const isInDemo = features.authMode == "DEMO";
+  const bannerText = isInDemo
+    ? "Demo mode is enabled. Do not share the app until Firebase auth is enabled."
+    : undefined;
+
   return (
-    <HeaderWithBar device="Both">
+    <HeaderWithBar bannerText={bannerText} device="Both">
       <div className="col-start-2 col-end-12 flex items-center justify-start">
         <h1 className="lg:hidden text-lg font-bold text-white">
           Ludocode Admin Hub
         </h1>
-        <AdminNavigationIconGroup/>
+        <AdminNavigationIconGroup />
       </div>
     </HeaderWithBar>
   );
