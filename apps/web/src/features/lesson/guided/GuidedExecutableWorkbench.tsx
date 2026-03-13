69568c88c31a05c8ef931d28f1b5a6e81c959ef5
Dave <davidglogowski2003@gmail.com>
2026-03-13 16:26:39 +0100

    remove unused imports from guided executable workbench

--- apps/web/src/features/lesson/guided/GuidedExecutableWorkbench.tsx
+++ apps/web/src/features/lesson/guided/GuidedExecutableWorkbench.tsx
@@ -1,17 +1,8 @@
-import { LudoButton } from "@ludocode/design-system/primitives/ludo-button.tsx";
-import { LudoTab } from "@ludocode/design-system/primitives/tab.tsx";
-import { cn } from "@ludocode/design-system/cn-utils";
-import { BlockRenderer } from "@ludocode/design-system/widgets/exercise/BlockRenderer";
-import { Workbench } from "@ludocode/design-system/widgets/Workbench";
 import { useHotkeys } from "@ludocode/hooks";
-import { qo } from "@/queries/definitions/queries.ts";
 import { useLessonContext } from "@/features/lesson/context/useLessonContext";
-import { ProjectProvider } from "@/features/project/workbench/context/ProjectContext.tsx";
 import { useProjectContext } from "@/features/project/workbench/context/ProjectContext.tsx";
-import { CodeRunnerProvider } from "@/features/project/workbench/context/CodeRunnerContext.tsx";
 import { useCodeRunnerContext } from "@/features/project/workbench/context/CodeRunnerContext.tsx";
 import { useFeatureEnabledCheck } from "@/features/auth/hooks/useFeatureEnabledCheck";
-import { ProjectEditor } from "@/features/project/workbench/editor/ProjectEditor.tsx";
 import { WorkbenchOutputPane } from "@/features/project/workbench/output/WorkbenchOutputPane.tsx";
 import { stripFileName } from "@/features/project/util/filenameUtil.ts";
 import { evaluateExecutableTests } from "../util/executableTestUtil";
@@ -33,7 +24,7 @@ export function GuidedExecutableWorkbench({
     stageExecutableAttempt,
     handleExerciseButtonClick,
   } = useLessonContext();
-  const { files, current, setCurrent } = useProjectContext();
+  const { files } = useProjectContext();
   const { runCode, outputInfo } = useCodeRunnerContext();
   const runnerFeature = useFeatureEnabledCheck({ feature: "isPistonEnabled" });
   const { isRunning, outputLog } = outputInfo;
