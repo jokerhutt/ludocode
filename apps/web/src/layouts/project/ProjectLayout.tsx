9875f7c7cedadba4339627d9641c7e1c13ab550a
Dave <davidglogowski2003@gmail.com>
2026-03-20 14:19:48 +0100

    make currentUser optional in projectLayout

--- apps/web/src/layouts/project/ProjectLayout.tsx
+++ apps/web/src/layouts/project/ProjectLayout.tsx
@@ -3,33 +3,40 @@ import { WorkbenchPage } from "@/features/project/workbench/WorkbenchPage.tsx";
 import { getRouteApi, useLoaderData } from "@tanstack/react-router";
 import { ProjectHeader } from "@/features/project/workbench/zones/ProjectHeader.tsx";
 import { MainGridWrapper } from "@ludocode/design-system/layouts/grid/main-grid-wrapper.tsx";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { qo } from "@/queries/definitions/queries";
 import { UserPreferencesContext } from "@/features/user/context/useUserPreferenceContext.tsx";
+import type { UserPreferences } from "@ludocode/types";
 
 type ProjectLayoutProps = {};
 
 export type ProjectMode = "EDIT" | "READONLY";
 
 export function ProjectLayout({}: ProjectLayoutProps) {
-  const routeApi = getRouteApi(
-    "/_desktopguard/project/$authorId/$projectId",
-  );
+  const routeApi = getRouteApi("/_desktopguard/project/$authorId/$projectId");
   const { project } = useLoaderData({ from: routeApi.id });
-  const { data: currentUser } = useSuspenseQuery(qo.currentUser());
+  const { data: currentUser } = useQuery(qo.currentUser());
+  const { data: UserPreferences } = useQuery(qo.preferences());
+
+  const preferences: UserPreferences = UserPreferences ?? {
+    userId: "",
+    hasExperience: false,
+    chosenPath: {careerType: "DATA", title: "", description: "", topPath: "",},
+    aiEnabled: false,
+    audioEnabled: true,
+  };
+
   const { authorId } = routeApi.useParams();
 
-  const mode: ProjectMode = authorId === currentUser.id ? "EDIT" : "READONLY";
-  const isReadOnly = mode === "READONLY" || !!project.deleteAt
-
-  const { data: UserPreferences } = useSuspenseQuery(qo.preferences());
+  const mode: ProjectMode = authorId === currentUser?.id ? "EDIT" : "READONLY";
+  const isReadOnly = mode === "READONLY" || !!project.deleteAt;
 
   return (
-    <UserPreferencesContext.Provider value={UserPreferences}>
+    <UserPreferencesContext.Provider value={preferences}>
       <ProjectProvider project={project}>
         <MainGridWrapper className="max-h-dvh min-h-0" gridRows="SITE">
           <ProjectHeader mode={mode} />
-          <WorkbenchPage readOnly={isReadOnly}/>
+          <WorkbenchPage readOnly={isReadOnly} />
         </MainGridWrapper>
       </ProjectProvider>
     </UserPreferencesContext.Provider>
