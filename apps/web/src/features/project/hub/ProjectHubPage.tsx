a6d194e119ff59383153557e1421ed12ac2deb5d
Dave <davidglogowski2003@gmail.com>
2026-03-20 19:54:02 +0100

    adjust mobile project hub page paddings

--- apps/web/src/features/project/hub/ProjectHubPage.tsx
+++ apps/web/src/features/project/hub/ProjectHubPage.tsx
@@ -51,8 +51,8 @@ export function ProjectHubPage() {
   return (
     <>
       <div className="layout-grid col-span-full scrollable py-6 px-8 lg:px-0">
-        <div className="col-span-1" />
-        <div className="relative col-span-10 flex flex-col gap-6 justify-start min-w-0">
+        <div className="col-span-1 hidden lg:block" />
+        <div className="relative col-span-full lg:col-span-10 flex flex-col gap-6 justify-start min-w-0">
           <Hero {...projectHeroContent}>
             <div className="flex items-center gap-4">
               <span
